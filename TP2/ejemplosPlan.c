f0f2c5817ef8cd5aa2f850b35737f1bf6e6599d4
EmmaDolz <emmadolz@Emma.Emma>
2025-04-09 16:06:22 -0300

    ej 3 y 4

--- TP2/ejemplosPlan.c
+++ TP2/ejemplosPlan.c
@@ -58,4 +58,52 @@ tiempo espera de p4 = 13
 tiempo espera de p5 = 4
 
 valor medio de espera = (8+1+0+13+4)/5 = 5,2
+
+PRIORITY SCHEDULIND no-apropiativo
+(0-5)p1, (5-9)p5, (9-10)p3, (10-17)p4, (17-20)p2
+
+turnaround para p1 = 5
+turnaround para p2 = 20
+turnaround para p3 = 10
+turnaround para p4 = 17
+turnaround para p5 = 9
+
+valor medio de turnaround = (5+20+10+17+9)/5 = 12,2
+
+tiempo espera de p1 = 0
+tiempo espera de p2 = 17
+tiempo espera de p3 = 9
+tiempo espera de p4 = 10
+tiempo espera de p5 = 5
+
+valor medio de espera = (0+17+9+10+5)/5 = 8,2
+
+ROUND-ROBIN (quantum = 2)
+(0-2)p1, (2-4)p2, (4-5)p3, (5-7)p4, (7-9)p5, (9-11)p1, (11-12)p2, (12-14)p4, (14-16)p5, 
+(16-17)p1, (17-19)p4, (19-20)p4
+
+turnaround para p1 = 17
+turnaround para p2 = 12
+turnaround para p3 = 5
+turnaround para p4 = 20
+turnaround para p5 = 16
+
+valor medio de turnaround = (17+12+5+20+16)/5 = 14
+
+tiempo espera de p1 = 17-5 = 12
+tiempo espera de p2 = 12-3 = 9
+tiempo espera de p3 = 4
+tiempo espera de p4 = 20-7 = 13
+tiempo espera de p5 = 16-4 = 12
+
+valor medio de espera = (12+9+4+13+12)/5 = 10
+
+VALOR MINIMO DE TIEMPO DE ESPERA 
+FCFS tuvo un medio de espera de: 7
+SJF tuvo un medio de espera de: 5,2
+PRIORITY SCHEDULING tuvo un medio de espera de 8,2
+ROUND-ROBIN tuvo un medio de espera de: 10
+
+por lo tanto... el algoritmo que tuvo el menor tiempo de 
+espera fue el shortest job first
 */
